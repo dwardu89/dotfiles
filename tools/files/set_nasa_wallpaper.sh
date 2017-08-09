8a33fc58c051742eb1394cc7df7a5debed2bcef4
Edward Vella <eddvella89@gmail.com>
2017-08-09 18:28:41 +0100

    Fixed a bug in the set nasa wallpaper script where the image wasnt being set after it changes, so the actual filename is used

--- tools/files/set_nasa_wallpaper.sh
+++ tools/files/set_nasa_wallpaper.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -v
+set -e
 JQ=/usr/local/bin/jq
-DOWNLOAD_PATH='/tmp/nasa_image.jpg'
+DOWNLOAD_PATH='/tmp/'
 API_KEY='DEMO_KEY'
 NASA_URL="https://api.nasa.gov/planetary/apod?api_key=${API_KEY}"
 NASA_IMAGE_HD=$(curl --retry 10 --retry-delay 20 "${NASA_URL}" | ${JQ} --raw-output '.hdurl')
 
-curl --retry 10 --retry-delay 20 "${NASA_IMAGE_HD}" -o ${DOWNLOAD_PATH}
+DOWNLOAD_PATH="${DOWNLOAD_PATH}${NASA_IMAGE_HD##*/}"
+ 
+curl "${NASA_IMAGE_HD}" -o ${DOWNLOAD_PATH}
 
 osascript -e 'tell application "System Events" to set picture of every desktop to ("'"${DOWNLOAD_PATH}"'" as POSIX file as alias)'
 rm ${DOWNLOAD_PATH}
