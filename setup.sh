4dee9987b64d5c65bad0f51a9d9a147e9256ae2b
Edward Vella <hello@dwardu.com>
2019-11-25 21:13:53 +0000

    Created a requirements yaml file to manually create required jobs, added a download script.sh

--- setup.sh
+++ setup.sh
@@ -1,50 +1,6 @@
 #! /bin/bash
-run_setup () {
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REQUIREMENTS_FILE=requirements.json
 
-  # source all shell scripts
-  for shell_script in "${DIR}/scripts/"*.sh;
-  do
-    echo "${shell_script}"
-    source "${shell_script}"
-  done
-
-  initial_setup
-  ask_details
-  sync_icloud
-  update_system
-
-  # Languages and Utilities
-  install_brew
-  install_python
-  install_node
-
-  # Applications
-
-  install_brew_apps
-  install_cask_apps
-  install_air_vpn_client
-  install_aws_related
-
-  # Configure
-  configure_zsh
-  configure_oh_my_zsh
-  configure_git
-  configure_iterm_2
-  install_atom_packages
-
-  # MacOS Configuration
-  configure_macos
-
-  # Update dotfiles
-  backup_existing_dotfiles
-  symlink_dotfiles
-
-  # Cleanup
-  cleanup_brew
-  cleanup_error_log
-}
-
-# run and log errors to file (but still show them when they happen)
-readonly error_log="${HOME}/Desktop/install_errors.log"
-run_setup 2> >(tee "${error_log}")
+jq -r '.taps[]' ${REQUIREMENTS_FILE} | xargs -t -I {} sh -c 'brew tap "$1"' - {}
+jq -r '.formulae[]' ${REQUIREMENTS_FILE} | xargs -t -I {} sh -c 'brew install "$1"' - {}
+jq -r '.cask[]' ${REQUIREMENTS_FILE} | xargs -t -I {} sh -c 'brew cask install "$1"' - {}
