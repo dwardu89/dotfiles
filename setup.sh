ab0a9f337218b3d01bf3d78a46fa0042bc529007
Edward Vella <hello@dwardu.com>
2020-01-10 20:42:41 +0000

    Adding a comment as a todo

--- setup.sh
+++ setup.sh
@@ -10,3 +10,5 @@ cd ${HOME}/dotfiles
 ls dotfiles -1 | xargs  -I % sh -c 'stow -d ~/dotfiles/dotfiles -t ${HOME} %'
 
 brew bundle
+
+# https://pawelgrzybek.com/sync-vscode-settings-and-snippets-via-dotfiles-on-github/
